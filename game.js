32a854d20d8974980f9c4034cc8dd53969dc1e4e
Nicole Ampofo <30731402+nicoleampofo@users.noreply.github.com>
2020-07-18 14:54:11 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,15 @@
+//create secretNumber
+var secretNumber = 4;
+
+//ask user to guess
+var StringGuess = (prompt("Guess a number"));
+var guess = Number(stringGuess);
+
+//check if guess is correct
+if(guess === secretNumber) {
+	alert('You got it right!');
+} else if (guess > secretNumber) {
+	alert("That number is too high. Refresh the page to try again.");
+} else {
+	alert("That number is too low. Refresh the page to try again.");
+}
